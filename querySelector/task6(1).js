384ec0a998b81e25f8655a25c0ca5dab4d77a3ca
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-05-10 11:50:35 +0530

    queryselector task

---
+++ querySelector/task6(1).js
@@ -0,0 +1,5 @@
+var seconditem=document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.backgroundColor='Green';
+var thirditem=document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.color='white';
+//thirditem.remove();
